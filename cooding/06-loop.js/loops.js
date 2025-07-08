7e77aafeb40d71883be4cb69bb79efa7320c5361
khaderayyan <khaderayyan675@gmail.com>
2025-07-08 06:15:43 +0000

    todays class work 07-08-25

---
+++ cooding/06-loop.js/loops.js
@@ -0,0 +1,48 @@
+// basics loops in js //
+
+// for loop
+// while loop
+// do while loop
+// map
+// for in loop
+// for in loop
+
+//for (j = 1 ; j<= 10 ; j++){
+//    for(let i=1 ; i<=10 ; i++){
+//       console.log(`${i}*${j}=${i*j}`)
+//    }
+//}
+
+
+// var readline=require("readline-sync");
+// var table=readline.questionInt("table start: ")
+// var endTable=readline.questionInt("table end: ")
+// var tableRows =readline.questionInt("enter how many rows to print in each table : ")
+
+// for(let i=table;i<=endTable;i++ ){
+//     console.log(`________Table: ${i}________`);
+//     for(let j=1;j<=5;j++){
+//         console.log(i,j,i*j);
+//     }
+// }
+
+
+
+// var readline=require("readline-sync");
+//  var table=readline.questionInt("table start: ")
+//  var endTable=readline.questionInt("table end: ")
+//  //var tableRows =readline.questionInt("enter how many rows to print in each table : ")
+
+//  for(let i=table;i<=endTable;i++ ){
+//      console.log(`________Table: ${i}________`);
+//      for(let j=1;j<= 10;j++){
+//          console.log(i,j,i*j);
+//      }
+//    }
+
+// var output=" "
+// for ( i=0 ; i<4 ; i++){
+//     output = output+'*'
+// }
+
+// console.log(output) 
---
+++ cooding/08-matrices.js/input-matrix.js
@@ -0,0 +1,13 @@
+// import readlinesync from 'readline-sync';
+
+// function matrixInput(){
+//     console.log('enter rows and coloumns: ');
+//     const rows = readlinesync.questionInt("Enter rows: ");
+//     const coloumns = readlinesync.questionInt("Enter coloumns: ");
+
+//     const matrix = new Array(rows);
+//     for (let i = 0; i<matrix.length;i++){
+//         matrix[i] = new array(coloumns);
+
+//     }
+// }
